c7978ff88cc647e1cbf54b2f950777b2ccfa0c1f
rpgoldberg <rpgoldberg@gmail.com>
2025-05-19 17:59:58 -0500

    corrected error unknown issues for strict typechecking

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -64,7 +64,7 @@ export const admin = async (req: Request, res: Response, next: NextFunction) => 
         message: 'Not authorized as admin'
       });
     }
-  } catch (error) {
+  } catch (error: unknown) {
     res.status(500).json({
       success: false,
       message: 'Server Error',
